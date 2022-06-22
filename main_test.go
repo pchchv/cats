880cf9c8f27ede423868a8f5ff9ac3fedbe188f6
Evgenii Pochechuev <ipchchv@gmail.com>
2022-06-22 17:26:42 +0300

    Add test GET request with parameters

--- main_test.go
+++ main_test.go
@@ -69,19 +69,29 @@ func testServerPing(t *testing.T) {
 	if string(body) != "Cats Service. Version 0.1\n" && string(body) != "Cats Service. Version 0.1" {
 		t.Fatal()
 	}
-}
+}Add test GET request without parametersAdd test GET request without parameters
 
 func testServer(t *testing.T) {
-	res, err := http.Get(fmt.Sprintf("localhost:8080/cats"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	if res.StatusCode != http.StatusOK {
-		t.Errorf("status not OK")
-	}
-	defer res.Body.Close()
-	_, err = ioutil.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal(err)
+	params := []string{"",
+		"?attribute=color",
+		"?attribute=color&order=desc",
+		"?offset=7",
+		"?limit=4",
+		"?offset=5&limit=2",
+		"?attribute=color&limit=3",
+		"?attribute=color&order=asc&offset=5&limit=2"}
+	for _, v := range params {
+		res, err := http.Get(fmt.Sprintf("localhost:8080/cats" + v))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if res.StatusCode != http.StatusOK {
+			t.Errorf("status not OK")
+		}
+		_, err = ioutil.ReadAll(res.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		res.Body.Close()
 	}
 }
